8c2e3895d28ffeef25434788d31d7c57e4ed2687
Manuel Haro <manuelharo1994@gmail.com>
2016-10-26 13:45:17 -0500

    Se agregaron validaciones para crear usuario y actualizar usuario

--- resources/views/usuario/edit.blade.php
+++ resources/views/usuario/edit.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 
+@include('alerts.request')
 {!!Form::model($user, ['route' => ['usuario.update', $user->id], 'method' => 'PUT'])!!}
     @include('usuario.forms.usr')
   {!!Form::submit('Actualizar', ['class' => 'btn btn-primary'])!!}
