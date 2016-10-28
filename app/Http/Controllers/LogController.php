be7c03d98b82cb87356bd45c7a7dacc52d1a0418
Manuel Haro <manuelharo1994@gmail.com>
2016-10-27 23:15:41 -0500

    Se agrego soft-deleting al model usuario

---
+++ app/Http/Controllers/LogController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Cinema\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Cinema\Http\Requests;
+
+class LogController extends Controller
+{
+    //
+}
--- app/User.php
+++ app/User.php
@@ -3,11 +3,13 @@
 namespace Cinema;
 
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -26,6 +28,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    protected $dates = ['deleted_at'];
 
     public function setPasswordAttribute($valor) {
       if(!empty($valor)) {
